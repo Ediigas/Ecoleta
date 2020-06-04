85f401e2ff2c932726169ee37381e1a5eafa1acf
Douglas Silva <douglas.edig@outlook.com>
2020-06-03 22:01:28 -0300

    oday we add the collection items and locations where you can collect the items and put a lot of functionality in javaScript

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -2,12 +2,12 @@ function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
 
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then( res => res.json() )
-    .then( states => {
-        for( const state of states) {
-            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-        }    
-    })
+        .then(res => res.json())
+        .then(states => {
+            for (const state of states) {
+                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+            }
+        })
 }
 
 populateUFs()
@@ -24,15 +24,21 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios`
 
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
+
     fetch(url)
-    .then( res => res.json() )
-    .then( cities => {
-        for( const city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
-        } 
-        
-        citySelect.disabled = false
-    })
+        .then(res => res.json())
+        .then(cities => {
+
+            for (const city of cities) {
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+            }
+
+            citySelect.disabled = false
+        })
 }
 
 
@@ -40,4 +46,58 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+
+
+//Itens de coleta
+//pegar todos os li´s
+const itemsToCollect = document.querySelectorAll('.items-grid li')
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", hanleSelectedItem)
+
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+
+let selectedItems = []
+
+
+function hanleSelectedItem(event) {
+    const itemLi = event.target
+
+    //ADD OU REMOVE uma classe com javascript
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+
+
+    //Verificar se existem items selecionado, se sim
+    //pegar os items selecionados
+    const alreadySelected = selectedItems.findIndex(item => {
+        const itemFound = item == itemId //isso será true ou false
+        return itemFound
+    })
+
+    // se ja estiver selecionado, 
+    if (alreadySelected >= 0) {
+        //tirar da selecao
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId //false
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    }else{
+        // se nao estiver selecionado,
+        //adicionar na seleção
+        selectedItems.push(itemId)
+    }
+
+    //atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+}
+
 
